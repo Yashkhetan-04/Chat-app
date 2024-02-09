d07291948275816ecdc5a2459295a3eecb1761bd
Yashkhetan-04 <yashkhetan04@gmail.com>
2024-02-09 16:06:05 +0530

    Chat-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Chat from './components/Chat'
+import SignUp from './components/SignUp'
+import Login from './components/Login'
+import SetAvatar from './components/SetAvatar'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          
+          <Route path='/' element={<Chat/>} />
+          <Route path='/signup' element={<SignUp/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/setAvatar' element={<SetAvatar/>} />
+          
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
