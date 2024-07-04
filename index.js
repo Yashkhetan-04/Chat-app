1021e6d663f30c0f3b804ff86747eb1ea87b01eb
Yashkhetan-04 <yashkhetan04@gmail.com>
2024-07-04 14:22:53 +0530

    new latest

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ const server = app.listen(PORT);
 
 const io = socket(server , {
     cors:{
-        origin:"http://localhost:3000",
+        origin: process.env.ORIGIN,
         credentials:true,
     }
 })
